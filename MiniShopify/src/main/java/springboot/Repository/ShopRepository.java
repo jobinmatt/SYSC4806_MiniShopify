576788340db5896ab8bb885e7e4fbaac888a6567
rjbansal <rajatb9703@gmail.com>
2020-02-19 10:09:43 -0500

    #5 fixes with repository

--- MiniShopify/src/main/java/springboot/Repository/ShopRepository.java
+++ MiniShopify/src/main/java/springboot/Repository/ShopRepository.java
@@ -1,12 +1,11 @@
 package springboot.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import springboot.model.Shop;
+import java.util.List;
 
-@Repository
 public interface ShopRepository extends JpaRepository<Shop, Long>{
 
-    Shop findByName(String name);
+    List<Shop> findByName(String name);
     Shop findById (long id);
 }
