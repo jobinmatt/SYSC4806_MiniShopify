3d8095bf05ffce713a034a5537f9593002beb556
Shounak Amladi <shounakamladi@cmail.carleton.ca>
2020-02-26 14:18:19 -0500

    intergrated Vue.js into Spring Backend

--- MiniShopify/backend/src/main/java/springboot/controllers/MainVueController.java
+++ MiniShopify/backend/src/main/java/springboot/controllers/MainVueController.java
@@ -1,13 +1,15 @@
 package springboot.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class MainVueController {
 
     @GetMapping("/")
     public String index(){
-        return "Hello";
+
+        return "index";
     }
 }
