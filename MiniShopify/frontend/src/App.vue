8732b27ac4faf269bfd88b7c3e2dc7d67793f1e5
Shounak Amladi <shounakamladi@cmail.carleton.ca>
2020-02-27 14:42:40 -0500

    aligning branch with master

--- MiniShopify/frontend/src/App.vue
+++ MiniShopify/frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1> Mini-Shopify </h1>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
--- MiniShopify/frontend/src/main.js
+++ MiniShopify/frontend/src/main.js
@@ -2,10 +2,28 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+import VueRouter from 'vue-router'
+import Dashboard from './pages/Dashboard'
+import SignUp from './pages/SignUp'
+import Cart from './pages/Cart'
+import Error from './pages/Error'
+import ShopPage from './pages/ShopPage'
+import Login from './pages/Login'
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+const routes = [
+  { path: '/', component: Dashboard },
+  { path: '/signup', component: SignUp },
+  { path: '/cart', component: Cart },
+  { path: '/error', component: Error },
+  { path: '/shop', component: ShopPage },
+  { path: '/login', component: Login }
+]
+
+const router = new VueRouter({
+  routes
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
