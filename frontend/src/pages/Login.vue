212934f3c6a1a64a7a94f22f05d5e3c5a40f256a
Shounak Amladi <shounakamladi@cmail.carleton.ca>
2020-03-09 16:32:34 -0400

    missed this change, fixes the test

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -3,8 +3,8 @@
     <icon></icon><h1>Mini-Shopify</h1>
     <h4>LOGIN</h4>
     <h4>Kickstart Your Business Today!</h4>
-    <input type="text" placeholder="Email"><br>
-    <input type="text" placeholder="Password"><br>
+    <input class="loginform" type="text" placeholder="Email"><br>
+    <input class="loginform" type="password" placeholder="Password"><br>
     <button>Login</button>
   </div>
 </template>
