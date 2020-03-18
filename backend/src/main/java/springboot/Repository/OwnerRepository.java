f1c8aca0ead50ba473f446ab7240d96d32adde6e
rjbansal <rajatb9703@gmail.com>
2020-03-18 17:46:55 -0400

    Fix #65 #66 #67

--- backend/src/main/java/springboot/Repository/OwnerRepository.java
+++ backend/src/main/java/springboot/Repository/OwnerRepository.java
@@ -11,6 +11,7 @@ public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
     List<Owner> findByFirstName(@Param("firstName") String firstName);
     List<Owner> findByLastName(@Param("lastName") String lastName);
+    Owner findByEmail(@Param("email") String email);
     List<Owner> findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
     Owner findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 }
--- backend/src/main/java/springboot/controller/OwnerController.java
+++ backend/src/main/java/springboot/controller/OwnerController.java
@@ -1,5 +1,6 @@
 package springboot.controller;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,30 +29,72 @@ public class OwnerController {
         if(newOwner != null && !isEmptyNull(newOwner.getFirstName()) && !isEmptyNull(newOwner.getLastName()) && !isEmptyNull(newOwner.getEmail())
                 && newOwner.getPassword() != null){
 
-            Owner user = new Owner();
+            Owner user = ownerRepo.findByEmail(newOwner.getEmail());
+            if(user != null){
+                return ResponseEntity.badRequest().body("Email already exists");
+            }
+
+            user = new Owner();
             user.setFirstName(newOwner.getFirstName());
             user.setLastName(newOwner.getLastName());
             user.setEmail(newOwner.getEmail());
-            user.setPassword(newOwner.getPassword());
+            user.setPassword(BCrypt.hashpw(newOwner.getPassword(), BCrypt.gensalt()));
 
             ownerRepo.save(user);
-            return ResponseEntity.ok(user); //Everything is OK so we send the okay response with the new shop in the body
+            return ResponseEntity.ok().body("User added"); //Everything is OK so we send the okay response with the new shop in the body
         } else {
             return ResponseEntity.badRequest().body("Owner object was null");
         }
     }
 
-    @GetMapping(value = "/api/owner")
-    public ResponseEntity getOwner(@RequestParam(value = "ownerID") Long ownerID){
+    @GetMapping(value = "/api/login")
+    public ResponseEntity login (@RequestBody OwnerDTO owner) {
+        if (owner != null && !isEmptyNull(owner.getEmail()) && owner.getPassword() != null) {
+
+            Owner user = ownerRepo.findByEmail(owner.getEmail());
+
+            if (user == null) {
+                return ResponseEntity.badRequest().body("Email not found");
+            }
+            if (BCrypt.checkpw(owner.getPassword(), user.getPassword())) {
+                return ResponseEntity.ok().body("Login Successful");
+            } else {
+                return ResponseEntity.badRequest().body("Wrong password");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Owner email and password is missing");
+        }
+    }
+
+    @GetMapping(value = "/api/owner/getFromId")
+    public ResponseEntity getOwnerFromID(@RequestParam(value = "ownerID") Long ownerID){
         if(ownerID != null){
             Optional<Owner> user = ownerRepo.findById(ownerID);
             if(user.isPresent()){
-                return ResponseEntity.ok(user);
+                Owner returnUser = user.get();
+                returnUser.setPassword("");
+                return ResponseEntity.ok(returnUser);
             } else {
                 return ResponseEntity.badRequest().body("Owner ID: " + ownerID + " not found");
             }
         } else {
             return ResponseEntity.badRequest().body("Owner ID is empty");
+        }
+    }
+
+    @GetMapping(value = "/api/owner/getFromEmail")
+    public ResponseEntity getOwnerFromEmail(@RequestParam(value = "email") String email){
+        if(email != null){
+            Owner user = ownerRepo.findByEmail(email);
+            if(user != null){
+                Owner returnUser = user;
+                returnUser.setPassword("");
+                return ResponseEntity.ok(returnUser);
+            } else {
+                return ResponseEntity.badRequest().body("Owner email: " + email + " not found");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Owner email is empty");
         }
     }
 
