bc1066a891949134928883303fed5326097b7b95
jobinmatt <jobinmatt@live.ca>
2020-04-02 02:14:16 -0400

    fixed styling of navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -71,7 +71,7 @@
     padding-left: 2%;
     padding-right: 5%;
     background: #DDECFF;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
     border-radius: 30px;
     justify-content: space-between;
   }
@@ -99,11 +99,12 @@
   }
 
   .signup_button {
+    color: white;
     padding: 8px 12px;
     margin: auto;
     background: #007DC4;
+    border: #007DC4;
     border-radius: 8px;
-    box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.1);
-    color: white;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
   }
 </style>
